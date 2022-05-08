71a8750cb5e63a551ae223f80f91239925bd8209
Lionel Rigoux <lionel.rigoux@sf.mpg.de>
2022-05-08 19:10:22 +0200

    clean

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM docker:dind
-
-RUN mkdir -p /tmpdata
-
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod u+x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Publishing: ${INPUT_SOURCE}"
-
-docker run -v "${GITHUB_WORKSPACE}:/data/:rw" registry.gitlab.com/pandemics/docker publish
-
-DIR="$(dirname "${INPUT_SOURCE}")/pandemics"
-echo "::set-output name=pandemics-dir::${DIR}"
